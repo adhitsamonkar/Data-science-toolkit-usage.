b8b2eeb6a935664e68830b99b927c9b598c2e71c
adhitsamonkar <140580127+adhitsamonkar@users.noreply.github.com>
2025-11-18 12:01:44 +0530

    Add pie chart for confirmed cases by WHO region
    Added a pie chart to visualize the share of confirmed COVID-19 cases by WHO region.

--- Lab_sea_2304.R
+++ Lab_sea_2304.R
@@ -38,4 +38,22 @@ ggplot(region_data, aes(x = reorder(WHO.Region, TotalConfirmed),
        x = "WHO Region",
        y = "Total Confirmed Cases") +
   theme_minimal(base_size = 14)
+#pie chart
+# Aggregate by region
+region_data <- data %>%
+  group_by(WHO.Region) %>%
+  summarise(TotalConfirmed = sum(Confirmed, na.rm = TRUE))
+
+# Pie chart
+ggplot(region_data, aes(x = "", y = TotalConfirmed, fill = WHO.Region)) +
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y") +
+  labs(title = "Share of Confirmed Cases by WHO Region",
+       fill = "WHO Region") +
+  theme_minimal() +
+  theme(axis.title = element_blank(),
+        axis.text = element_blank(),
+        panel.grid = element_blank())
+
+
 
